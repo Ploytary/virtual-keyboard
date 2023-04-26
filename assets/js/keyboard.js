6f62edc712151cff1ec049d23cd1a4029af627ec
Dima Grigorev <firebrand-rsschool@rambler.ru>
2023-04-26 20:11:03 +0300

    feat: add keyboard component click listener

--- assets/js/keyboard.js
+++ assets/js/keyboard.js
@@ -7,6 +7,10 @@ export default class KeyboardComponent {
     this.outputField = outputField;
     this.keyComponents = null;
     this.element = null;
+    this.capsLock = false;
+    this.shiftKey = false;
+    this.ctrlKey = false;
+    this.altKey = false;
   }
 
   init() {
@@ -41,6 +45,8 @@ export default class KeyboardComponent {
       .filter((keyData) => keyData.order !== undefined)
       .sort((a, b) => a.order - b.order)
       .map((keyData) => new KeyComponent(keyData));
+
+    this.setClickListener();
   }
 
   render(container) {
@@ -66,5 +72,247 @@ export default class KeyboardComponent {
     });
 
     this.container.append(this.element);
+  }
+
+  setClickListener() {
+    this.element.addEventListener('click', (evt) => {
+      const keyButton = evt.target.closest('.key');
+      let printValue = null;
+      if (keyButton) {
+        switch (keyButton.dataset.details) {
+          case 'Tab': printValue = '    ';
+            break;
+          case 'Enter': printValue = '\n';
+            break;
+          case 'MetaLeft': printValue = '';
+            break;
+          case 'Backspace':
+            this.deleteChar('backward');
+            printValue = '';
+            break;
+          case 'NumpadDecimal':
+            this.deleteChar('forward');
+            printValue = '';
+            break;
+          case 'CapsLock':
+            this.switchKeyHoldStatus(keyButton);
+            printValue = '';
+            break;
+          case 'ShiftLeft':
+          case 'ShiftRight':
+            this.switchKeyHoldStatus(keyButton);
+            printValue = '';
+            break;
+          case 'ControlLeft':
+          case 'ControlRight':
+            this.switchKeyHoldStatus(keyButton);
+            printValue = '';
+            break;
+          case 'AltLeft':
+          case 'AltRight':
+            this.switchKeyHoldStatus(keyButton);
+            printValue = '';
+            break;
+          case 'ArrowUp':
+            this.moveCaret(-1, 0);
+            printValue = '';
+            break;
+          case 'ArrowDown':
+            this.moveCaret(1, 0);
+            printValue = '';
+            break;
+          case 'ArrowLeft':
+            this.moveCaret(0, -1);
+            printValue = '';
+            break;
+          case 'ArrowRight':
+            this.moveCaret(0, 1);
+            printValue = '';
+            break;
+          default:
+            printValue = keyButton.textContent;
+        }
+        this.insertChar(printValue);
+        this.updateKeys();
+        this.outputField.focus();
+      }
+
+      return undefined;
+    });
+  }
+
+  moveCaret(verticalSteps = 0, horizontalSteps = 0) {
+    const charLines = this.outputField.value.split('\n');
+    const currentCaretAbsolutePosition = this.outputField.selectionStart;
+    const {
+      caretLinePosition,
+      caretInlinePosition,
+    } = this.getCaretRelativePosition(currentCaretAbsolutePosition);
+
+    const forwardLine = charLines[caretLinePosition + verticalSteps];
+    if (forwardLine === undefined) {
+      return undefined;
+    }
+
+    let newCaretPosition = currentCaretAbsolutePosition;
+    if (caretInlinePosition === charLines[caretLinePosition].length) {
+      if (verticalSteps < 0) {
+        newCaretPosition = newCaretPosition - caretInlinePosition - 1;
+      }
+      if (verticalSteps > 0) {
+        newCaretPosition = newCaretPosition + charLines[caretLinePosition + 1].length + 1;
+      }
+      newCaretPosition += horizontalSteps;
+    } else {
+      let index = 0;
+      newCaretPosition = 0;
+      while (index !== caretLinePosition + verticalSteps) {
+        newCaretPosition += charLines[index].length + 1;
+        index += 1;
+      }
+      if (caretInlinePosition <= forwardLine.length) {
+        newCaretPosition += caretInlinePosition + horizontalSteps;
+      } else {
+        newCaretPosition += forwardLine.length;
+      }
+    }
+
+    newCaretPosition = newCaretPosition < 0 ? 0 : newCaretPosition;
+    this.outputField.selectionStart = newCaretPosition;
+    this.outputField.selectionEnd = newCaretPosition;
+    return undefined;
+  }
+
+  getCaretRelativePosition(absolutePosition) {
+    const charLines = this.outputField.value.split('\n');
+    let step = absolutePosition;
+    let index = 0;
+    for (let i = 0; i < charLines.length; i += 1) {
+      if (step > charLines[i].length) {
+        step -= charLines[i].length + 1;
+        index = i + 1;
+      } else {
+        break;
+      }
+    }
+    return { caretLinePosition: index, caretInlinePosition: step };
+  }
+
+  deleteChar(direction = 'backward') {
+    let shift;
+    switch (direction) {
+      case 'backward':
+        shift = -1;
+        break;
+      case 'forward':
+        shift = 0;
+        break;
+      default:
+        shift = -1;
+        break;
+    }
+
+    let caret = this.outputField.selectionStart + shift;
+    if (caret >= 0) {
+      this.outputField.value = [this.outputField.value.slice(0, caret), this.outputField.value.slice(caret + 1)].join('');
+    } else {
+      caret = 0;
+    }
+
+    this.outputField.selectionStart = caret;
+    this.outputField.selectionEnd = caret;
+  }
+
+  insertChar(printValue) {
+    if (printValue === '') {
+      return undefined;
+    }
+    const currentCaretAbsolutePosition = this.outputField.selectionStart;
+    const charLines = this.outputField.value.split('\n');
+    const {
+      caretLinePosition,
+      caretInlinePosition,
+    } = this.getCaretRelativePosition(currentCaretAbsolutePosition);
+    const workLine = charLines[caretLinePosition];
+    const newLine = [...workLine.slice(0, caretInlinePosition), printValue, ...workLine.slice(caretInlinePosition)].join('');
+    charLines[caretLinePosition] = newLine;
+    this.outputField.value = charLines.join('\n');
+
+    let index = 0;
+    let newCaretPosition = 0;
+    while (charLines[index] !== newLine) {
+      newCaretPosition += charLines[index].length + 1;
+      index += 1;
+    }
+    newCaretPosition += caretInlinePosition + printValue.length;
+
+    this.outputField.selectionStart = newCaretPosition;
+    this.outputField.selectionEnd = newCaretPosition;
+    return undefined;
+  }
+
+  switchKeyHoldStatus(key) {
+    switch (key.dataset.details) {
+      case 'ShiftLeft':
+      case 'ShiftRight': this.shiftKey = !this.shiftKey;
+        break;
+      case 'ControlLeft':
+      case 'ControlRight': this.ctrlKey = !this.ctrlKey;
+        break;
+      case 'AltLeft':
+      case 'AltRight': this.altKey = !this.altKey;
+        break;
+      case 'CapsLock': this.capsLock = !this.capsLock;
+        break;
+      default: return undefined;
+    }
+
+    return undefined;
+  }
+
+  switchKeyHoldClass(searchKeyCode, keyStatus) {
+    this.keyComponents
+      .filter((component) => component.code === searchKeyCode)
+      .map((component) => component.getElement())
+      .forEach((element) => {
+        if (keyStatus) {
+          element.classList.add('virtual-keyboard__key--hold');
+        } else {
+          element.classList.remove('virtual-keyboard__key--hold');
+        }
+      });
+    return undefined;
+  }
+
+  updateKeys() {
+    const mayHoldedKeyList = [
+      { keyCode: 16, keyStatusSource: this.shiftKey },
+      { keyCode: 17, keyStatusSource: this.ctrlKey },
+      { keyCode: 18, keyStatusSource: this.altKey },
+      { keyCode: 20, keyStatusSource: this.capsLock },
+    ];
+
+    mayHoldedKeyList.forEach(({ keyCode, keyStatusSource }) => {
+      this.switchKeyHoldClass(keyCode, keyStatusSource);
+    });
+
+    if (this.shiftKey) {
+      this.keyComponents.forEach((component) => {
+        let buttonValue = null;
+        const element = component.getElement();
+        if (Array.isArray(component.value)) {
+          buttonValue = component.value.length > 1 ? component.value[1] : component.value[0];
+          element.textContent = buttonValue;
+        } else {
+          element.textContent = component.value;
+        }
+      });
+    } else {
+      this.keyComponents.forEach((component) => {
+        const buttonValue = Array.isArray(component.value) ? component.value[0] : component.value;
+        const element = component.getElement();
+        element.textContent = buttonValue;
+      });
+    }
   }
 }
